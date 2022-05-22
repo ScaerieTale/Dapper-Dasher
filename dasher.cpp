5953b5b5e63e7a85f377baecf1d858bddf29ef62
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-05-21 23:03:01 -0500

    More file setup to get the basic window running

--- dasher.cpp
+++ dasher.cpp
@@ -2,5 +2,25 @@
 
 int main()
 {
-    
-}
+    // Initial window setup
+    int width{800};
+    int height{450};
+    InitWindow(width, height, "Dapper Dasher");
+    SetTargetFPS(60);
+
+    // Initiate game loop
+    while(WindowShouldClose() != true)
+    {
+        // In-game window setup
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        // Game logic begin
+
+
+
+
+        // Game logic end
+        EndDrawing();
+    };
+};
