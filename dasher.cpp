e61b3ff0d467ceab7607436aded4b05b8e35c73c
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-05-21 23:47:45 -0500

    Implemented basic velocity

--- dasher.cpp
+++ dasher.cpp
@@ -3,16 +3,17 @@
 int main()
 {
     // Initial window setup
-    const int width{512};
-    const int height{380};
-    InitWindow(width, height, "Dapper Dasher");
+    const int window_width{512};
+    const int window_height{380};
+    InitWindow(window_width, window_height, "Dapper Dasher");
     SetTargetFPS(60);
     
     // Rectangle Dimensions - FOR SCIENCE!!
-    const int rect_width{40};
-    const int rect_height{40};
-    int rect_x{200};
-    int rect_y{0};
+    const int rect_width{50};
+    const int rect_height{80};
+    int rect_x{window_width / 2};
+    int rect_y{window_height - rect_height};
+    int rect_velocity{-10};
 
     // Initiate game loop
     while(!WindowShouldClose())
@@ -23,6 +24,7 @@ int main()
 
         // Game logic begin
         DrawRectangle(rect_x, rect_y, rect_height, rect_width, BLUE);
+        rect_y += rect_velocity;
 
 
 
