26ba1d4a86b6762ead887861ebe790dd594dfd2e
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-05-22 01:11:34 -0500

    PHYSICS IS WORKING!  Took me about 45 minutes to debug because I had + and - (because Y is difficult like that) ass-backwards, but it's working now

--- dasher.cpp
+++ dasher.cpp
@@ -7,13 +7,16 @@ int main()
     const int window_height{380};
     InitWindow(window_width, window_height, "Dapper Dasher");
     SetTargetFPS(60);
+
+    // Acceleration (pixels/frame)/frame
+    const int gravity{1};
     
     // Rectangle Dimensions - FOR SCIENCE!!
-    const int rect_width{50};
-    const int rect_height{80};
-    int rect_x{window_width / 2};
-    int rect_y{window_height - rect_height};
-    int rect_velocity{0};
+    const int width{80};
+    const int height{50};
+    int pos_x{window_width / 2};
+    int pos_y{window_height - height};
+    int velocity{0};
 
     // Initiate game loop
     while(!WindowShouldClose())
@@ -23,17 +26,28 @@ int main()
         ClearBackground(WHITE);
 
         // Game logic begin
-        DrawRectangle(rect_x, rect_y, rect_height, rect_width, BLUE);
-        // Press SPACE to jump!
-        if (IsKeyPressed(KEY_SPACE) && rect_y > 0)
+        DrawRectangle(pos_x, pos_y, width, height, BLUE);
+
+        // Physics
+        // Check for ground
+        if (pos_y >= window_height - height)
         {
-            rect_velocity -= 20;
-            rect_y += rect_velocity;
+            // On the ground
+            velocity = 0;
         }
-        else if (!IsKeyPressed(KEY_SPACE) && rect_y < window_height - rect_height)
+        else
         {
-            rect_y += 5;
-            };
+            // In mid-air, apply gravity
+            velocity -= gravity;
+        }
+        // Spacebar to jump!
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            velocity += 10;
+        }
+
+        // Update position
+        pos_y -= velocity;
 
 
 
